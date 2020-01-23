cfc94a0276aab6f3dd821d9b41ab1ebcd465bc1e
laura stevenson <laurastevenson@lauras-MacBook-Pro.local>
2020-01-23 12:51:56 -0800

    completed challenge

---
+++ buy-sell-stock.py
@@ -0,0 +1,28 @@
+# find lowest element in list
+# then find highest element that comes after it 
+# return difference of higest and lowest
+
+
+def find_max_profit(lst):
+	"""Given a list of stock prices by day
+	calculate max profit possible"""
+
+	lowest_inx = 0
+
+	for i in range(len(lst)):
+		if lst[i] < lst[lowest_inx]:
+			lowest_inx = i
+
+	highest = lst[lowest_inx]
+
+	for price in lst[lowest_inx:]:
+		if price > highest:
+			highest = price
+
+	if highest - lst[lowest_inx] < 0:
+		return 0
+
+	return highest - lst[lowest_inx]
+
+print(find_max_profit([7,1,5,3,6,4]))
+print(find_max_profit([7,6,4,3,1]))
